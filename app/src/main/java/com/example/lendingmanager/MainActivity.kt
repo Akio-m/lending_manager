413bc0e295cf4f10c81287f7cc46a86bd8122a7d
k168577@ie.u-ryukyu.ac.jp <k168577@ie.u-ryukyu.ac.jp>
2019-06-10 01:02:36 +0900

    create create layout.

--- app/src/main/java/com/example/lendingmanager/MainActivity.kt
+++ app/src/main/java/com/example/lendingmanager/MainActivity.kt
@@ -5,17 +5,23 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        // create realm
+        realm = Realm.getDefaultInstance()
+
+        add.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
@@ -31,8 +37,17 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.menu_history -> true
             R.id.menu_mail -> true
-            R.id.menu_finish -> moveTaskToBack(true)
+            R.id.menu_finish -> {
+                finish()
+                toast(R.string.toast_finish)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
